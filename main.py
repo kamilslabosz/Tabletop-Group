22b31110618ddab07b1c0659be94a68e9ba53c77
Kamil <slaboszkamil@gmail.com>
2022-03-22 09:20:39 +0100

    Small design changes css changes picture resizing

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 Bootstrap(app)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # db = SQLAlchemy(app)
 load_dotenv()
