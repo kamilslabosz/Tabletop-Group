fe8732eeee8ae113367d86b97010fac1abe4af92
Kamil <slaboszkamil@gmail.com>
2022-04-28 13:57:23 +0200

    changed root.js to jQuery

--- static/js/root.js
+++ static/js/root.js
@@ -1,23 +1,14 @@
-var vagabond = document.querySelectorAll(".root-faction-btn")[6]
-var factions = document.querySelectorAll(".faction-div")
-
-document.getElementById("riverfolk").addEventListener("click", function(){
-    if (vagabond.innerHTML === "Vagabond"){
-        vagabond.innerHTML = "Vagabond (both)";
-    } else {
-        vagabond.innerHTML = "Vagabond";
-    }
-    factions[4].classList.toggle('hidden');
-    factions[5].classList.toggle('hidden');
-    factions[6].classList.toggle('hidden');
+$('#riverfolk').click(function(){
+   $('.rf').toggleClass('hidden');
+   $(".vgbd").click(function(){
+     $(".vgbd2").toggleClass('hidden');
+   });
 });
 
-document.getElementById("underworld").addEventListener("click", function(){
-    factions[7].classList.toggle('hidden');
-    factions[8].classList.toggle('hidden');
+$('#underworld').click(function(){
+   $('.uw').toggleClass('hidden');
 });
 
-document.getElementById("marauder").addEventListener("click", function(){
-    factions[9].classList.toggle('hidden');
-    factions[10].classList.toggle('hidden');
+$('#marauder').click(function(){
+   $('.mrdr').toggleClass('hidden');
 });
